cdde17214a9aa86ae216e78a25dab8ec5c4fc4d6
shruti.mohapatra <shruti.mohapatra@incture.com>
2022-05-05 10:44:11 +0530

    hii

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import './App.css';
 import Login from './views/Login/Login';
 import SignUp from './views/SignUp/SignUp';
 import Demo from './views/dashboard/Table';
+import Home from './views/C-Dashboard/Home';
 
 
 function App() {
   return (
     <div className="App">
     <div>
-     <Login />
+     {/* <Login /> */}
      </div>
    {/* <SignUp></SignUp> */}
    {/* <Demo></Demo> */}
+   <Home></Home>
+   <Home></Home>
+   
     </div>
   );
 }
